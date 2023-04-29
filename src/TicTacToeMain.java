2b58a6a30de7dacb9e292a347a807a8d7dcda599
Ashish Pal <ak987982@gmail.com>
2023-04-29 22:14:58 +0530

    Added UC1 and Created Tic Tac Toe Game Board

--- src/TicTacToeMain.java
+++ src/TicTacToeMain.java
@@ -1,5 +1,6 @@
 public class TicTacToeMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe Game Simulation");
+        TicTacToeGame game = new TicTacToeGame();
+        game.displayBoard();
     }
 }
